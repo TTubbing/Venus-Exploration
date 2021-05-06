f300315094fda14324ff840254070e5a5e6cdad7
Marijn982 <58325858+Marijn982@users.noreply.github.com>
2021-05-06 19:40:03 +0200

    Update Moving_basic.c

--- Moving_basic.c
+++ Moving_basic.c
@@ -1,13 +1,12 @@
 #include <Servo.h>                           
 
-int pinLeft = 12;
-int pinRight = 13;
+int pinLeft = 12; // Digital pin for the left motor
+int pinRight = 13; // Digital pin for the right motor
 
-int encoder = 0;
-int timeturn = 2000;
-int correction = -1;
+int timeturn = 2000; // Total time needed to turn 360 degree
+int correction = -1; // Correction for the motors
 
-Servo servoLeft;
+Servo servoLeft; // Define servos
 Servo servoRight;
 
 void setup() {
@@ -17,38 +16,38 @@ void setup() {
 }
 
 void loop() {
-  int angle;
+  int angle; // Define angle which robot has to turn
 
-  forward();
+  forward(); // Move forwards
+  delay(5000); // Keep moving forwards during delay
+  stopp(); // Stop moving
   delay(5000);
-  stopp();
-  delay(5000);
-  backward();
+  backward(); //Move backwards
   delay(5000);
   stopp();
   delay(5000);
   angle = 90;
-  turnRight(angle);
+  turnRight(angle); // Robot turns to the right (90 degree)
   delay(5000);
   angle = 90;
-  turnLeft(angle);
+  turnLeft(angle); // Robot turns to the left (90 degree)
   delay(5000);
 }
 
 
 
 void forward(){
-  servoLeft.write(180);
-  servoRight.write(0);
+  servoLeft.write(180); // Send signal to servo
+  servoRight.write(0); // 0 fast backwards, 90 stop, 180 fast forwards.
 }
 
 void backward(){
-  servoLeft.write(0);
+  servoLeft.write(0); // Left is 0 and right 180 because of the way installed
   servoRight.write(180);
 }
 
 void stopp(){
-  servoLeft.write(90+correction);
+  servoLeft.write(90+correction); // Correction is needed to calibrate stop point
   servoRight.write(90+correction);
 }
 
@@ -56,10 +55,10 @@ void turnRight(int angle){
   
   int milliseconds;
   
-  milliseconds = (360/angle)*timeturn;
+  milliseconds = (360/angle)*timeturn; // seconds to turn the given angle
   servoLeft.write(180);
   servoRight.write(180);
-  delay(milliseconds);
+  delay(milliseconds); // During the delay the motors keep running till next line
   stopp();
 }
 
