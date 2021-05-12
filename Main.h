d0f1ff92fcf0fc303ced55ed7dc94747fa5bf691
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-12 15:40:48 +0200

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,20 +1,31 @@
-int cliff, edge = 0; //infrared detectable obstacles
-int distance = 0; //distance to obstacle
-int min_distance = 10; //Minimum distance to an obstacle
+int IR = 1; //infrared detectable obstacles
+int US_high, US_low = 0; //distance to obstacle
+int mountain, cliff, rock = 0;
+int min_distance = 15; //Minimum distance to an obstacle
 int lab = 1; //Robot at lab
+int starting_sequence = 1;
 
 void setup() {
   
 }
 
 void loop() {
-  if(cliff == 1){
+  forward(); //drive forward
+  
+  IR = ir_input() //read infrared input
+  US_high = US_high_input() //read input from high ultrasound sensor
+  US_low = US_low_input() //read input from low ultrasound sensor
+  
+  if(US_low <= min_distance){ //if distance to obstacle is minimum
+    if(US_high <= min_distance){ //check if high ultrasound sensor is same
+      mountain = 1; //then mountain
+    }
+    else{
+      rock = 1; //else rock
+    }
+  }
+  
+  if(IR == 0){
     
-  }
-  
-  if(edge == 1){
-  }
-  
-  if(distance < min_distance){
   }
 }
