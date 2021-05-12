2c1b84c0ff3e55a49bc342f8b81ac36b52f4afca
TTubbing <31215650+TTubbing@users.noreply.github.com>
2021-05-12 15:19:48 +0200

    Update Encoders.h

--- Encoders.h
+++ Encoders.h
@@ -1,3 +1,5 @@
+#include <EnableInterrupt.h>
+
 int encoderLeft = 7;
 int encoderRight = 8;
 volatile int count_left, rounds_left = 0;
@@ -5,13 +7,8 @@ volatile int count_right, rounds_right = 0;
 float diameter_wheel = 5;
 float distance_left, distance_right = 0;
 
-void setup_decoders(){
-  pinMode(encoderLeft, INPUT);
-  pinMode(encoderRight, INPUT);
-  attachInterrupt(digitalPinToInterrupt(encoderLeft), isrLeft ,RISING); //attach function for left encoder
-  attachInterrupt(digitalPinToInterrupt(encoderRight), isrRight ,RISING); //attach function for right encoder
-  
-}
+volatile uint16_t interruptCountLeft=0; // The count will go back to 0 after hitting 65535.
+volatile uint16_t interruptCountRight=0; // The count will go back to 0 after hitting 65535.
 
 void calculate_distance(){
   distance_left = rounds_left*PI*diameter_wheel; //calculate distance travelled by left wheel
@@ -20,18 +17,25 @@ void calculate_distance(){
 
 //attach function to get rounds for left encoder
 void isrLeft(){
-  count_left++; //increase count per high signal
-  if(count_left == 9){ //if count is 9 made full circle
-    rounds_left++;
-    count_left = 0;
-  }
+  interruptCountLeft++;
 }
 
 //attach function to get rounds for right encoder
 void isrRight(){
-  count_right++; //increase count per high signal
-  if(count_right == 9){//if count is 9 made full circle
-    rounds_right++;
-    count_right=0;
-  }
+  interruptCountRight++;
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(encoderLeft, INPUT_PULLUP);
+  enableInterrupt(encoderLeft, isrLeft, CHANGE);
+  pinMode(encoderRight, INPUT_PULLUP);
+  enableInterrupt(encoderRight, isrRight, CHANGE);
+}
+
+// In the loop we just display interruptCount. The value is updated by the interrupt routine.
+void loop() {
+  delay(1000);
+  Serial.println(interruptCountLeft, DEC);
+  Serial.println(interruptCountRight, DEC);
 }
