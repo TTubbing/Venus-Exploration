8ae07ac4670a99f4857236fef2046b28b64504f2
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-12 15:49:43 +0200

    Update Encoders.h

--- Encoders.h
+++ Encoders.h
@@ -2,40 +2,31 @@
 
 int encoderLeft = 7;
 int encoderRight = 8;
-volatile int count_left, rounds_left = 0;
-volatile int count_right, rounds_right = 0;
+volatile int count_left = 0;
+volatile int count_right = 0;
 float diameter_wheel = 5;
 float distance_left, distance_right = 0;
 
-volatile uint16_t interruptCountLeft=0; // The count will go back to 0 after hitting 65535.
-volatile uint16_t interruptCountRight=0; // The count will go back to 0 after hitting 65535.
+void setup_encoders() {
+  Serial.begin(115200);
+  pinMode(encoderLeft, INPUT_PULLUP);
+  enableInterrupt(encoderLeft, isrLeft, RISING);
+  pinMode(encoderRight, INPUT_PULLUP);
+  enableInterrupt(encoderRight, isrRight, RISING);
+}
 
 void calculate_distance(){
-  distance_left = rounds_left*PI*diameter_wheel; //calculate distance travelled by left wheel
-  distance_right = rounds_right*PI*diameter_wheel; // calculate distance travelles by right wheel
+  distance_left = (rounds_left/9)*PI*diameter_wheel; //calculate distance travelled by left wheel
+  distance_right = (rounds_right/9)*PI*diameter_wheel; // calculate distance travelles by right wheel
+  rounds_left, rounds_right = 0;
 }
 
 //attach function to get rounds for left encoder
 void isrLeft(){
-  interruptCountLeft++;
+  count_left++;
 }
 
 //attach function to get rounds for right encoder
 void isrRight(){
-  interruptCountRight++;
-}
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(encoderLeft, INPUT_PULLUP);
-  enableInterrupt(encoderLeft, isrLeft, CHANGE);
-  pinMode(encoderRight, INPUT_PULLUP);
-  enableInterrupt(encoderRight, isrRight, CHANGE);
-}
-
-// In the loop we just display interruptCount. The value is updated by the interrupt routine.
-void loop() {
-  delay(1000);
-  Serial.println(interruptCountLeft, DEC);
-  Serial.println(interruptCountRight, DEC);
+  count_right++;
 }
