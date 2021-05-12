a0cbbbdf05da503b10dc7cde1e4338724c3d8d58
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-12 15:04:36 +0200

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,6 +1,20 @@
-void setup{
+int cliff, edge = 0; //infrared detectable obstacles
+int distance = 0; //distance to obstacle
+int min_distance = 10; //Minimum distance to an obstacle
+int lab = 1; //Robot at lab
+
+void setup() {
+  
 }
 
-void loop{
+void loop() {
+  if(cliff == 1){
+    
+  }
   
+  if(edge == 1){
+  }
+  
+  if(distance < min_distance){
+  }
 }
