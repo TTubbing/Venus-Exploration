aabf4c6fb55956e0247b72204ac1894917db7fe6
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-19 16:05:02 +0200

    Update path.h

--- path.h
+++ path.h
@@ -1,14 +1,14 @@
 struct movement { //Structure that holds each movement command
   int angle; //angle of movement
-  int distance; //distance of movement
+  int count; //distance of movement
 };
 
 int pathlength = 0; //length of path commands
 
 struct movement path[20]; //path array
 
-void addpath(int angle, int distance){ //add path to path array
+void addpath(int angle, int count){ //add path to path array
   path[pathlength].angle = angle; //add angle
-  path[pathlength].distance = distance; //add distance
+  path[pathlength].count = count; //add distance
   pathlength++; //increase length of path commands
 }
