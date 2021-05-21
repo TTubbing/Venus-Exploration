7ffd6ca1d1c9b8151cf2f0e107dc6a56d5bb8285
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-21 11:23:30 +0200

    Update a_Global.ino

--- ordered_code/a_Global.ino
+++ ordered_code/a_Global.ino
@@ -47,3 +47,13 @@ Servo RightmotorServo;
 //obstacle detection
 const int error_margin = 2;
 const int max_distance = 300;
+//path memory
+
+typedef struct movement { //Structure that holds each movement command
+  int angle; //angle of movement
+  int count; //distance of movement
+}movement;
+
+const int maxpath = 20;
+int pathlength = 0;
+movement path[maxpath]; //path array
