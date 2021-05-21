d300983612b72072f7cd7830e3996470eac6f9da
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-21 10:41:46 +0200

    Update path.c

--- path.c
+++ path.c
@@ -1,18 +1,30 @@
-struct movement { //Structure that holds each movement command
+typedef struct movement { //Structure that holds each movement command
   int angle; //angle of movement
   int count; //distance of movement
-};
+}movement;
 
-int pathlength = 0; //length of path commands
-
-struct movement path[20]; //path array
+const int maxpath = 20;
+int pathlength = 0;
+movement path[maxpath]; //path array
 
 void addpath(int angle, int count){ //add path to path array
-  path[pathlength].angle = angle; //add angle
-  path[pathlength].count = count; //add distance
-  pathlength++; //increase length of path commands
+	  path[pathlength].angle = angle; //add angle
+	  path[pathlength].count = count; //add distance
+	  pathlength++; //increase length of path commands
+	}
+	
+void initialise_path(){ //initialise all values in path array to 0
+    for(int i = 0; int i < maxpath; i++){
+        path[i].angle = 0;
+        path[i].count = 0;
+    }
 }
 
 void returnpath(){
-  for(
+    turn(180); //turn around
+    
+    for(int place = maxpath-1; place >= 0; place--){//loop backwards through path array
+        forward(path[place].count);//drive forward amount in path array
+        turn((path[place].angle)*-1);//drive opposite angle back
+    }
 }
