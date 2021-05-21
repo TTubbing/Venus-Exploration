411a78aec4ab3f2abea7d8b53ae20bb470b44b79
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-21 13:04:04 +0200

    Update b_Setup.ino

--- ordered_code/b_Setup.ino
+++ ordered_code/b_Setup.ino
@@ -3,4 +3,10 @@ void setup(){
   Serial.begin(9600);
   xbee.begin(9600);
   
+  //setup encoders
+  pinMode(encoderLeft, INPUT_PULLUP);
+  enableInterrupt(encoderLeft, isrLeft, RISING);
+  pinMode(encoderRight, INPUT_PULLUP);
+  enableInterrupt(encoderRight, isrRight, RISING);
+  
 }
