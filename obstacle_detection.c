0762de14ab0a4dec4646aeb651cfb9f681686721
Marijn982 <58325858+Marijn982@users.noreply.github.com>
2021-05-25 20:15:07 +0200

    Update obstacle_detection.c

--- obstacle_detection.c
+++ obstacle_detection.c
@@ -14,8 +14,8 @@ int detect_sample(int angle){
   for(int angle = 0; angle<=180; angle++){
     USservo.write(angle); //turn 1 degree
     
-    US_high = US_high(); //return distance from high ultrasound sensor
     US_low = US_low(); //return distance from low ultrasound sensor
+    US_high = US_high(); //return distance from high ultrasound sensor
     
     if(US_low < (US_high-error_margin) && US_low < max_distance){ //if rock sample is detected
       return angle; //return angle of rock sample
@@ -30,8 +30,8 @@ int detect_mountain(int angle){
   for(int angle = 0; angle<=180; angle++){
     USservo.write(angle); //turn 1 degree
     
-    US_high = US_high(); //return distance from high ultrasound sensor
     US_low = US_low(); //return distance from low ultrasound sensor
+    US_high = US_high(); //return distance from high ultrasound sensor
     
     if(US_low < (US_high+error_margin) && US_low > (US_high-error_margin) && US_low < max_distance && US_high < max_distance){ //if mountain is detected
       return angle; //return angle of rock sample
