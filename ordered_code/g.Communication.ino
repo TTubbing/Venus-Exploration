d39b20db5c0bfbd74799c7855ed8ad78992fd82d
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-05-26 12:35:00 +0200

    Create g.Communication.ino

---
+++ ordered_code/g.Communication.ino
@@ -0,0 +1,42 @@
+void send_data(int data){
+  Serial.print(data);
+ switch(data){
+  case 0:
+          at_lab = false;
+    break;
+    case 1:
+          at_lab = true;
+    break;
+    case 2:
+          rock_found = false;
+    break;
+    case 3:
+          rock_found = true;
+    break;
+}
+}
+
+void receive_data(){
+  if(xbee.available() > 0){
+    state = xbee.read();   
+  }
+  
+  /*if(Serial.available() > 0){
+      state = Serial.read();
+  }*/
+  switch(state){
+    case 0:
+          at_lab_other_robot = false;
+    break;
+    case 1:
+          at_lab_other_robot = true;
+    break;
+    case 2:
+          rock_found_other_robot = false;
+    break;
+    case 3:
+          rock_found_other_robot = true;
+    break;
+    
+  }
+}
