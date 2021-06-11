9a5caa87c65b016c6ad0ac5fdfba66fbdce235dc
Ward-GT <83354116+Ward-GT@users.noreply.github.com>
2021-06-11 10:36:54 +0200

    Add files via upload

---
+++ a_Global/c.Loop.ino
@@ -0,0 +1,4 @@
+void loop(){
+  stoppp();
+  mountain_function();
+}
---
+++ a_Global/e.Obstacle_detection.ino
@@ -0,0 +1,250 @@
+int detect_sample(){
+  long US_high, US_low;
+  Usservo.write(0);
+  delay(1000);
+  
+  for(int angle = 0; angle<=180; angle+=5){
+    Usservo.write(angle); //turn 5 degree
+    delay(100);
+    US_low = US_low_sensor(); //return distance from low ultrasound sensor
+    US_high = US_high_sensor(); //return distance from high ultrasound sensor
+    
+    if(US_low < (US_high-error_margin) && US_low < max_distance && US_low > distance_to_mountain){//if rock sample is detected
+      Usservo.write(angle+=5);
+      delay(500);
+      US_low = US_low_sensor(); //return distance from low ultrasound sensor
+      US_high = US_high_sensor(); //return distance from high ultrasound sensor
+      if(US_low < (US_high-error_margin) && US_low < max_distance && US_low > distance_to_mountain){
+      if (angle>5){
+        angle += 30;
+      }
+      return (-angle+90); //return angle of rock sample
+    }
+  }
+  }
+  return -1; //return -1 if no rock sample 
+}
+
+void drive_to_sample(int angle){
+   //declararion of older value US sensor
+  long old_US_low;
+  long US_low;
+  int IR_1;
+  int IR_2;
+  
+  turn(angle); // turn robot towards the mountain
+  Usservo.write(90);
+  
+  IR_1 = IR_1_sensor();
+  IR_2 = IR_2_sensor();
+
+  old_US_low = US_low_sensor();
+
+  US_low = old_US_low;
+  forward();
+  
+  while((old_US_low >= US_low - error_margin) && (US_low >= distance_to_mountain) && (IR_1 != 1) && (IR_2 != 1)){ // while the distance does not increase and the distance to the mountainis not to close drive forwards
+    old_US_low = US_low_sensor();
+
+    delay(update_interval);
+    
+    US_low = US_low_sensor();
+    
+    delay(update_interval);
+    
+    IR_1 = IR_1_sensor();
+    IR_2 = IR_2_sensor();
+  }
+
+  if(old_US_low <= US_low){ // if the older value of the US sensor is smaller than the new one it means it looks past the mountain and is on the wrong path.
+    stopp(0, count_right); // stopp and save distance travveled
+    angle = detect_sample(); // search turn angle to get back on track
+    drive_to_sample(angle);
+    return;
+  }
+  if(IR_1 == 1 || IR_2 ==1){
+    avoid_crater();
+    angle = detect_sample();
+    drive_to_sample(angle);
+  }
+  stopp(0, count_right); // distance is lower or equal to minimal distance to mountain so stop and save distance travelled
+}
+
+int detect_mountain(){
+   long US_high, US_low;
+   Usservo.write(0); //turn 1 degree
+   delay(1000);
+  for(int angle = 0; angle<=180; angle+=5){
+    Usservo.write(angle); //turn 1 degree
+    delay(50);
+    US_low = US_low_sensor(); //return distance from low ultrasound sensor
+    US_high = US_high_sensor(); //return distance from high ultrasound sensor
+    
+    if((US_low < (US_high+error_margin)) && (US_low > (US_high-error_margin)) && (US_low < max_distance) && (US_high < max_distance)){ //if mountain is detected
+      if (angle>5){
+        angle += 30;
+      }
+      return (-angle+90); //return angle of rock sample
+    }
+  }
+  return -1; //return -1 if no mountain
+}
+
+void avoid_crater(){
+  int angle = 180;
+  turn(180);
+  forward();
+  delay(1000); //for 10 cm
+  stopp(0, count_right);
+  turn(-90);
+  forward();
+  delay(4000); //parallel to crater
+  stopp(0, count_right);
+  turn(-90);
+}
+
+int IR_1_sensor(){
+  return 0;
+}
+
+int IR_2_sensor(){
+  return 0;
+}
+
+void drive_to_mountain(int angle){
+  
+  long old_US_high; //declararion of older value US sensor
+  long old_US_low;
+  long US_high;
+  long US_low;
+  int IR_1;
+  int IR_2;
+  
+  turn(angle); // turn robot towards the mountain
+  Usservo.write(90);
+  
+  IR_1 = IR_1_sensor();
+  IR_2 = IR_2_sensor();
+
+  old_US_low = US_low_sensor();
+  old_US_high = US_high_sensor();
+
+  US_low = old_US_low;
+  US_high = old_US_high;
+  forward();
+  
+  while((old_US_high >= US_high - error_margin) && (US_high >= distance_to_mountain) && (IR_1 != 1) && (IR_2 != 1)){ // while the distance does not increase and the distance to the mountainis not to close drive forwards
+    old_US_low = US_low_sensor();
+    old_US_high = US_high_sensor();
+
+    delay(update_interval);
+    
+    US_low = US_low_sensor();
+    US_high = US_high_sensor();
+    
+    delay(update_interval);
+    
+    IR_1 = IR_1_sensor();
+    IR_2 = IR_2_sensor();
+  }
+
+  if(old_US_high <= US_high && old_US_low <= US_low){ // if the older value of the US sensor is smaller than the new one it means it looks past the mountain and is on the wrong path.
+    stopp(0, count_right); // stopp and save distance travveled
+    angle = detect_mountain(); // search turn angle to get back on track
+    drive_to_mountain(angle);
+    return;
+  }
+  if(IR_1 == 1 || IR_2 ==1){
+    avoid_crater();
+    angle = detect_mountain();
+    drive_to_mountain(angle);
+  }
+  stopp(0, count_right); // distance is lower or equal to minimal distance to mountain so stop and save distance travelled
+}
+
+
+int detect_track(){
+   long US_high, US_low;
+  
+  for(int angle = 90-width; angle<=90+width; angle++){ // US servo turns in range of 2 times width to find mountain
+    Usservo.write(angle); //turn 1 degree
+    
+    US_low = US_low_sensor(); //return distance from low ultrasound sensor
+    US_high = US_high_sensor(); //return distance from high ultrasound sensor
+    
+    if(US_low < (US_high+error_margin) && US_low > (US_high-error_margin) && US_low < max_distance && US_high < max_distance){ //if mountain is detected
+      return angle; //return angle of rock sample
+    }
+  }
+  return -1; //return -1 if no mountain
+}
+
+int turn_away(){
+  long US_high, US_low;
+  
+  for(int angle = 0; angle<=180; angle++){
+    Usservo.write(angle); //turn 1 degree
+    
+    US_low = US_low_sensor(); //return distance from low ultrasound sensor
+    US_high = US_high_sensor(); //return distance from high ultrasound sensor
+    
+    if(US_low > out_of_sight && US_high > out_of_sight){ //if mountain is not detected
+      return angle; //return angle of rock sample
+    }
+  }
+  return -1;
+}
+
+void mountain_function(){//function to drive to and around mountain
+  int angle = detect_mountain();
+  drive_to_mountain(angle);
+  drive_around_mountain();
+}
+
+void drive_around_mountain(){
+  not_detect_mountain(); //look where mountain ends
+  drive_past_mountain(); //drive past it
+  turn(-90); //turn 90 degrees
+  forward();
+  delay(1000);
+  drive_past_mountain(); //drive past it
+}
+
+void not_detect_mountain(){
+  long US_high;
+  long US_low;
+  int angle = 90;
+  
+   for(angle = 90; angle>=0; angle-=5){
+    Usservo.write(angle); //turn 5 degree
+    delay(50);
+    US_low = US_low_sensor(); //return distance from low ultrasound sensor
+    US_high = US_high_sensor(); //return distance from high ultrasound sensor
+    
+    if(US_high > distance_to_mountain + error_margin){ //if mountain is not detected
+      if (angle>5){
+        angle -= 45;
+      }
+       turn(-angle+90); //turn away
+       break;
+    }
+} 
+}
+
+void drive_past_mountain(){ //function to drive parallel to the mountain till it is passed
+  long US_high;
+  long US_low;
+  int angle = 90;
+
+  Usservo.write(180); //turn servo to the left
+  delay(1000);
+  forward();
+  US_high = 0;
+  while(US_high <= distance_to_mountain + 10){ //While mountain is detected
+    US_high = US_high_sensor();
+    Serial.print(US_high);
+    delay(update_interval);
+  }
+  delay(500);
+  stopp(0, count_right);
+}
